50b5b67d83b8c878313d478a48d9e18bee073a1c
Marcus Macedo <marcus.macedo@acad.pucrs.br>
2019-08-10 18:17:22 -0300

    add app name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/MY_APP_NAME_HERE'));
+app.use(express.static(__dirname + '/dist/test-angular'));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/test-angular/index.html'));
